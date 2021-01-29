c4733f143c6d1c616646d7f6a5e9cf508a74dbc0
iida l <iidaliina.u@gmail.com>
2021-01-29 22:44:26 +0200

    made game screen 0.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css';
+// import logo from 'static/images/logo192.png';
 
 const App = () => {
-  return(
-    <h1>Hello murderer</h1>
-  )
-}
+    return (
+        <div>
+            <h1>Hello murderer</h1>
+            {/* <img alt="somepng" src={'static/images/logo192.png'} /> */}
+        </div>
 
+    )
+}
 
 export default App;
