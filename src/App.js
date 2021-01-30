ff4c07c9df580b121a8aaa6fb9f9cf720bc36d10
noorarytila <rytilanoora@gmail.com>
2021-01-30 13:20:27 +0200

    made better version of drag and drop

--- src/App.js
+++ src/App.js
@@ -1,81 +1,15 @@
-import React, { Component } from "react"
-import Murder from "./Murder"
-
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
+import React, { Component } from 'react'
+import './App.css'
+import Board from './Board'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            bin: [
-                { id: 1, bodies: [] }
-            ]
-        }
-    }
-
-    componentDidMount = () => {
-        const bodies = []
-        for (let id = 1; id <= 1; id++) {
-            bodies.push({ id: id })
-        }
-
-        this.setState({
-            bin: [
-                { id: 1, bodies: bodies }
-            ]
-        })
-    }
-
-    removeBody = bodyId => {
-        var binId = null
-        this.setState(prevState => {
-            prevState.bin.forEach(bin => {
-                bin.bodies = bin.bodies.filter(body => {
-                    if (body.id === bodyId) {
-                        binId = bin.id
-                        return false
-                    } else {
-                        return true
-                    }
-                })
-            })
-
-            return {
-                bin: prevState.bin
-            }
-        })
-        return binId
-    };
-
-    addBody = (bodyId, binId) => {
-      let bin = this.state.bin
-      this.setState({ bin: bin })
-    }
-
-
-    render() {
-        return (
-            
-            <div style={layoutStyle}>
-                <DndProvider backend={HTML5Backend}>
-                    <Murder
-                        bin={this.state.bin}
-                        removeBody={this.removeBody}
-                        addTile={this.addBody}
-                    />
-                </DndProvider>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <Board />
+      </div>
+    );
+  }
 }
 
-const layoutStyle = {
-    display: "grid",
-    gridTemplateRows: `
-    200px
-  `
-}
-
-export default App
+export default App;
